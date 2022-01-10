cb4dcc8f207fa34bd16f816027b03e51b8d1a05b
Jvnyor <josias100junior@gmail.com>
2022-01-10 10:48:34 -0300

    first commit

---
+++ src/test/java/com/Jvnyor/catalogoveiculos/CatalogoVeiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Jvnyor.catalogoveiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoVeiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
