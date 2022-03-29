8cd2ab2062896c156788352c6e151fc4ae70ce34
Josias Junior <74216703+Jvnyor@users.noreply.github.com>
2022-03-29 15:24:12 -0300

    Remove not used methods

--- src/main/java/com/Jvnyor/catalogoveiculos/repository/CarroRepository.java
+++ src/main/java/com/Jvnyor/catalogoveiculos/repository/CarroRepository.java
@@ -1,7 +1,5 @@
 package com.Jvnyor.catalogoveiculos.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,7 +8,4 @@ import com.Jvnyor.catalogoveiculos.model.Carro;
 @Repository
 public interface CarroRepository extends JpaRepository<Carro, Long>{
 
-	List<Carro> findByNome(String nome);
-	List<Carro> findByMarca(String marca);
-	List<Carro> findByModelo(String modelo);
 }
